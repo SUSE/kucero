046c8fc43fd706fe44039413975d8e00d9df7f90
JenTing Hsiao <jenting.hsiao@suse.com>
2020-06-03 13:59:57 +0800

    Remove redundant code
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- pkg/pki/cert/cert.go
+++ pkg/pki/cert/cert.go
@@ -20,28 +20,7 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
-
-	capi "k8s.io/api/certificates/v1beta1"
 )
-
-// IsCertificateRequestApproved returns true if a certificate request has the
-// "Approved" condition and no "Denied" conditions; false otherwise.
-func IsCertificateRequestApproved(csr *capi.CertificateSigningRequest) bool {
-	approved, denied := GetCertApprovalCondition(&csr.Status)
-	return approved && !denied
-}
-
-func GetCertApprovalCondition(status *capi.CertificateSigningRequestStatus) (approved bool, denied bool) {
-	for _, c := range status.Conditions {
-		if c.Type == capi.CertificateApproved {
-			approved = true
-		}
-		if c.Type == capi.CertificateDenied {
-			denied = true
-		}
-	}
-	return
-}
 
 // ParseCSR decodes a PEM encoded CSR
 // Copied from https://github.com/kubernetes/kubernetes/blob/v1.18.0-beta.2/pkg/apis/certificates/v1beta1/helpers.go
